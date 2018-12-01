f61e54a6ac4f38f3e14be299e6ff23fc2e5772c4
julien Demarque <julien_demarque@hotmail.fr>
2018-12-02 00:03:32 +0200

    added Prismjs (#1)
    * added prism
    
    * removed prism, kept prismjs

--- web/src/pages/Question.js
+++ web/src/pages/Question.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { withRouteData } from 'react-static';
+import Prism from 'prismjs';
+import '../prism.css';
 
 class QuestionPage extends Component {
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  componentDidUpdate() {
+    Prism.highlightAll();
+  }
+
   render() {
     const { question, file } = this.props;
-
     return (
       <div>
         <h1 style={{ textAlign: 'center' }}>{question.title}</h1>
         <p>{question.description}</p>
-        <pre>{file}</pre>
+        <pre>
+          <code className="language-javascript">{file}</code>
+        </pre>
         <ul>
           {question.answers.map(a => (
             <li>{a.answer}</li>
