04abb3bd136e3391827119b42ba93d63615cd035
adrianwix <37629556+adrianwix@users.noreply.github.com>
2018-12-06 23:19:10 +0100

    Styled Categories (#11)
    * Basic style categories
    
    * link cover all Cell in categories

--- web/src/pages/Category.js
+++ web/src/pages/Category.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import { withRouteData, Link } from 'react-static';
+import Grid from '../presentational/Shared/Grid';
+import { Card, Heading, majorScale } from 'evergreen-ui';
+import Row from '../presentational/Shared/Row';
+import Cell from '../presentational/Shared/Cell';
 //
 
 const Category = ({ name, quizzes, category }) => {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>{name}</h1>
-      <ul>
+    <Grid>
+      <Row>
+        <Cell
+          marginY={majorScale(3)}
+          paddingY={majorScale(2)}
+          elevation={0}
+          background={'white'}
+          columns={12}
+          justifyContent={'center'}
+        >
+          <Heading size={900} is={'h1'} textAlign={'center'}>
+            {name}
+          </Heading>
+        </Cell>
+      </Row>
+      <Row>
         {quizzes.map((q, index) => (
-          <li key={index}>
-            <Link to={`/${category.category}/${q.key}`}>{q.title}</Link>
-          </li>
+          <Cell height={250} hoverElevation={3} columns={4} key={index}>
+            <Link
+              style={{
+                color: 'inherit',
+                textDecoration: 'none',
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              to={`/${category.category}/${q.key}`}
+            >
+              <Heading size={700}>
+                <Card>{q.title}</Card>
+              </Heading>
+            </Link>
+          </Cell>
         ))}
-      </ul>
-    </div>
+      </Row>
+    </Grid>
   );
 };
 
