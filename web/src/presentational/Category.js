7b0c854569c2262a222c9bea87129ce89a9f8322
julien Demarque <julien_demarque@hotmail.fr>
2018-12-09 14:53:09 +0200

    subcategory filtering with query parameter (#15)
    * implement subcategory filtering
    
    * forgot to add data to commit
    
    * subcategory filtering with query parameter
    
    * change with adrian suggestions

--- web/src/presentational/Category.js
+++ web/src/presentational/Category.js
@@ -5,6 +5,9 @@ import Row from './Shared/Row';
 import Cell from './Shared/Cell';
 import { Heading, majorScale } from 'evergreen-ui';
 import { Link } from 'react-static';
+import queryString from 'query-string';
+
+console.log('queryString', queryString);
 
 const colors = {
   CORRECT: {
@@ -33,58 +36,81 @@ const colors = {
  * @returns {*}
  * @constructor
  */
-const Category = ({ name, category, javascriptResults }) => {
-  const { quizzes } = category;
-  return (
-    <Grid>
-      <Row>
-        <Cell
-          marginY={majorScale(3)}
-          paddingY={majorScale(2)}
-          elevation={0}
-          background={'white'}
-          columns={12}
-          justifyContent={'center'}
-        >
-          <Heading size={900} is={'h1'} textAlign={'center'}>
-            {name}
-          </Heading>
-        </Cell>
-      </Row>
-      <Row>
-        {quizzes.map((q, index) => {
-          const { border, background } = colors[javascriptResults[q.key]];
-          return (
-            <Cell
-              style={{ border: `2px solid ${border}` }}
-              background={background}
-              borderRadius={majorScale(2)}
-              height={250}
-              hoverElevation={3}
-              columns={4}
-              key={index}
-            >
-              <Link
-                style={{
-                  color: 'inherit',
-                  textDecoration: 'none',
-                  width: '100%',
-                  height: '100%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-                to={`/${category.category}/${q.key}`}
-              >
-                <Heading size={700}>{q.title}</Heading>
-              </Link>
-            </Cell>
-          );
-        })}
-      </Row>
-    </Grid>
-  );
-};
+class Category extends React.Component {
+  handleFilter = event => {
+    const { history } = this.props;
+    const currentPath = history.location.pathname;
+    const filter = event.target.innerText;
+    const location = filter === 'all' ? `${currentPath}` : `${currentPath}?filter=${filter}`;
+    history.push(location);
+  };
+
+  render() {
+    const { name, category, javascriptResults, history } = this.props;
+    const { quizzes, subcategories } = category;
+    const parsed = queryString.parse(history.location.search);
+    return (
+      <Grid>
+        <Row>
+          <Cell
+            marginY={majorScale(3)}
+            paddingY={majorScale(2)}
+            elevation={0}
+            background={'white'}
+            columns={12}
+            justifyContent={'center'}
+          >
+            <Heading size={900} is={'h1'} textAlign={'center'}>
+              {name}
+            </Heading>
+          </Cell>
+        </Row>
+        <Row>
+          {subcategories.map(subcategory => (
+            <div key={subcategory}>
+              <button onClick={this.handleFilter}>{subcategory}</button>
+            </div>
+          ))}
+        </Row>
+        <Row>
+          {quizzes
+            .filter(q => {
+              return q.subcategory.indexOf(parsed.filter || '') >= 0;
+            })
+            .map((q, index) => {
+              const { border, background } = colors[javascriptResults[q.key]];
+              return (
+                <Cell
+                  style={{ border: `2px solid ${border}` }}
+                  background={background}
+                  borderRadius={majorScale(2)}
+                  height={250}
+                  hoverElevation={3}
+                  columns={4}
+                  key={index}
+                >
+                  <Link
+                    style={{
+                      color: 'inherit',
+                      textDecoration: 'none',
+                      width: '100%',
+                      height: '100%',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                    to={`/${category.category}/${q.key}`}
+                  >
+                    <Heading size={700}>{q.title}</Heading>
+                  </Link>
+                </Cell>
+              );
+            })}
+        </Row>
+      </Grid>
+    );
+  }
+}
 
 Category.propTypes = {
   name: PropTypes.string.isRequired,
